b191c5d9d010e5ef68720789f3c693da7ec9dee1
araggohnxd <matheusnagato300@gmail.com>
2022-04-13 14:24:17 -0300

    Bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 17:07:44 by maolivei          #+#    #+#             */
+/*   Updated: 2022/04/13 14:21:03 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+#endif
